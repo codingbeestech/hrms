34478c9816b4305ef391d367c09f2c85bf98b6a9
Naveen Arya <naveenarya758@gmail.com>
2025-09-26 18:14:18 +0530

    Add project files

---
+++ library/application/config/database.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$active_group  = 'default';
+$active_record = true;
+
+$db['default']['dsn']          = '';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'software_libraryuser22';
+$db['default']['password'] = '^Lt,&7(GHROe';
+$db['default']['database'] = 'software_librarydb22';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix']     = '';
+$db['default']['pconnect']     = false;
+$db['default']['db_debug']     = true;
+$db['default']['cache_on']     = false;
+$db['default']['cachedir']     = '';
+$db['default']['char_set']     = 'utf8';
+$db['default']['dbcollat']     = 'utf8_general_ci';
+$db['default']['swap_pre']     = '';
+$db['default']['encrypt']      = false;
+$db['default']['compress']     = false;
+$db['default']['autoinit']     = false;
+$db['default']['stricton']     = false;
+$db['default']['failover']     = array();
+$db['default']['save_queries'] = true;
